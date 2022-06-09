4270a8dfde89e68d6ef34bdf9d068ca6eddf750a
yaolya <galyagina.olya@gmail.com>
2022-06-09 15:02:02 +0300

    setJointMotorControlArray

--- triple_pendulum.py
+++ triple_pendulum.py
@@ -23,8 +23,12 @@ p.setGravity(0, 0, -g)
 bodyId = p.loadURDF("./triple_pendulum.urdf")
 
 jIdx = p.getNumJoints(bodyId)
+forces = [0.] * jIdx
+velocities = [0.] * jIdx
+jointIndices = []
 
 for i in range(jIdx):
+    jointIndices.append(i)
     joint_info = p.getJointInfo(bodyId, i)
     if joint_info[2] is p.JOINT_REVOLUTE:
         print("revolute ", i, " ", joint_info)
@@ -48,20 +52,20 @@ pos0 = [q0_fact, 0]
 log_time = [t]
 log_pos = [q0_fact]
 
-p.setJointMotorControl2(bodyIndex = bodyId,
-                        jointIndex = 1,
+p.setJointMotorControlArray(bodyIndex = bodyId,
+                        jointIndices = jointIndices,
                         controlMode = p.VELOCITY_CONTROL,
-                        targetVelocity = 0,
-                        force = 0)
+                        targetVelocities = velocities,
+                        forces = forces)
 while t <= maxTime:
     p.stepSimulation()
     pos = p.getJointState(bodyId, 1)[0]
     t += dt
 
-    p.setJointMotorControl2(bodyIndex = bodyId,
-                        jointIndex = 1,
+    p.setJointMotorControlArray(bodyIndex = bodyId,
+                        jointIndices = jointIndices,
                         controlMode = p.TORQUE_CONTROL,
-                        force = 0.1)
+                        forces = forces)
 
     log_pos.append(pos)
     log_time.append(t)
